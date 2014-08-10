ab2a08e7205f9dce691e45988bceddb266cb4bc4
Jaryn <jaryn.kubik@gmail.com>
2014-08-10 14:28:48 +0200

    neco

--- RazorEx/Core.cs
+++ RazorEx/Core.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using Ultima;
 
 [assembly: AssemblyTitle("RazorEx")]
-[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyVersion("3.0.2.*")]
 
 namespace RazorEx
 {
